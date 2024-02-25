765b6def081f7b665f40fb5808c8f926de210ccb
Niall1985 <dcunhaniall@gmail.com>
2024-02-25 17:34:37 +0530

    Modify home screen
    - added govt policies slider
    - added weather button
    - removed bottom navigation bar
    - removed weather and govt policies grid elements

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:FarmXpert/home/coming_soon.dart';
 import 'package:FarmXpert/misc/config.dart';
 import 'package:FarmXpert/store/main_store.dart';
@@ -17,13 +19,49 @@ class _HomeScreenState extends State<HomeScreen> {
     'FarmBook',
     'Community',
     'Marketplace',
-    'Weather',
-    'Govt. Policies',
+  ];
+
+  List<String> textSliderTexts = [
+    'Pradhan Mantri Kisan MaanDhan Yojana',
+    'Pradhan Mantri Fasal Bima Yojana (PMFBY)',
+    'Rastriya Krishi Vikas Yojana',
+    'Modified Interest Subvention Scheme (MISS)',
+    'Market Intervention Scheme and Price support Scheme (MIS-PSS)',
+    'Pashu Kisan Credit Card Scheme ',
+    'Agri Infrastructure Fund',
+    'Soil Health Card Scheme',
+    'PM Kisan Sampada Yojana',
   ];
 
   Map<String, Widget?> homeRoutes = {
     "Marketplace": const StoreScreen(),
   };
+
+  int _currentIndex = 0;
+  final PageController _pageController = PageController(initialPage: 0);
+  late final Timer _timer;
+
+  void _timerCallback(Timer timer) {
+    _currentIndex = ((_currentIndex + 1) % textSliderTexts.length);
+    _pageController.animateToPage(
+      _currentIndex,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 5), _timerCallback);
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,12 +90,13 @@ class _HomeScreenState extends State<HomeScreen> {
                   width: MediaQuery.of(context).size.width * 0.4,
                   height: MediaQuery.of(context).size.width * 0.4,
                   decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: config.chosenTheme.primaryColorLight,
-                      image: const DecorationImage(
-                        image: AssetImage("assets/images/farmer.png"),
-                        fit: BoxFit.cover,
-                      )),
+                    shape: BoxShape.circle,
+                    color: config.chosenTheme.primaryColorLight,
+                    image: const DecorationImage(
+                      image: AssetImage("assets/images/farmer.png"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
                 Text(
                   FirebaseAuth.instance.currentUser!.displayName ?? "User",
@@ -184,64 +223,112 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-      ),
-      body: GridView.builder(
-        itemCount: 6,
-        padding: const EdgeInsets.all(16),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 16,
-          mainAxisSpacing: 16,
-        ),
-        itemBuilder: (context, index) {
-          return Card(
-            elevation: 1,
-            shape: RoundedRectangleBorder(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: ClipRRect(
               borderRadius: BorderRadius.circular(15),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const PageNotFound()),
+                  );
+                },
+                child: Container(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  color: const Color.fromARGB(255, 196, 225, 240),
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount: textSliderTexts.length,
+                    itemBuilder: (context, index) {
+                      return Center(
+                        child: Text(
+                          textSliderTexts[index],
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
             ),
-            child: InkWell(
-              onTap: () {
-                // Navigate to the corresponding page when an image is tapped
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        homeRoutes[imageTexts[index]] ?? const PageNotFound(),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const PageNotFound()),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: const Color.fromARGB(255, 196, 225, 240),
+              minimumSize: Size(MediaQuery.of(context).size.width * 0.9, 50),
+            ),
+            child: const Text(
+              "Click here to check today's weather",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              itemCount: imageTexts.length,
+              padding: const EdgeInsets.all(16),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+              ),
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 1,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: InkWell(
+                    onTap: () {
+                      // Navigate to the corresponding page when an image is tapped
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              homeRoutes[imageTexts[index]] ??
+                              const PageNotFound(),
+                        ),
+                      );
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/images/home/${imageTexts[index]}.png",
+                          height: 80,
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          imageTexts[index],
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "SF Pro Display",
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    "assets/images/home/${imageTexts[index]}.png",
-                    height: 80,
-                  ),
-                  const SizedBox(height: 16),
-                  Text(
-                    imageTexts[index],
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "SF Pro Display",
-                    ),
-                  ),
-                ],
-              ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
